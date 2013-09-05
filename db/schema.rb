6ebe843184a9f78dfb26f5906bb4a1fb03bef08c
Colin Gray <colin.gray@gatech.edu>
2013-09-05 02:10:52 -0400

    Added Team [#55951370]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130903031357) do
+ActiveRecord::Schema.define(version: 20130905051511) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "athletes", force: true do |t|
     t.string   "name"
@@ -23,6 +26,12 @@ ActiveRecord::Schema.define(version: 20130903031357) do
     t.string   "name"
     t.datetime "start_time"
     t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
