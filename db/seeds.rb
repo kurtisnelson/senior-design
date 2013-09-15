15228c3231a1a3cbcbf8ce48c52f456681720290
rfahsel3 <ryan.fahsel3@gmail.com>
2013-09-15 17:07:17 -0400

    Finished the story [#56453374]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Team.create :name => "In State Baseball", :description => "These players are people that went to highschool in Georgia"
+Team.create :name => "Out of state baseball", :description => "These players are people that went to school outside of Georgia"
+User.create :username => "firewirefahsel", :email => "ryan.fahsel@gmail.com", :team_id => 1 
+User.create :username => "kelsonprime", :email => "ruby_guru@gmail.com", :team_id => 2
+User.create :username => "bwittenbrook", :email => "bradley.wittebrook@mail.com", :team_id => 1
+User.create :username => "cgray9", :email => "colin.gray@gmail.com", :team_id => 2
+
