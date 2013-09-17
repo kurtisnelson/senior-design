23dcc31a438ce9c26c71589a65a7885ab74e008a
Kurt Nelson <kurtisnelson@gmail.com>
2013-09-16 20:41:44 -0400

    Fix time-dependent test

--- spec/decorators/game_decorator_spec.rb
+++ spec/decorators/game_decorator_spec.rb
@@ -4,13 +4,13 @@ describe GameDecorator do
   subject{GameDecorator.decorate(game)}
   describe "#status_tag" do
     context "present game" do
-      let(:game) { FactoryGirl.build(:game, start_date: Date.today - 1.day) }
+      let(:game) { FactoryGirl.build(:game, start_date: Date.today - 2.days) }
 
       specify{subject.status_tag.should include(I18n.t('game.in_progress'))}
     end
 
     context "future game" do
-      let(:game) { FactoryGirl.build(:game, start_date: Date.today + 1.day) }
+      let(:game) { FactoryGirl.build(:game, start_date: Date.today + 2.days) }
 
       specify{subject.status_tag.should include(I18n.t('game.scheduled'))}
     end
