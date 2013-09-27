4764ae57ba674faf23838d633f831d6af82bd271
rfahsel3 <ryan.fahsel@gmail.com>
2013-09-26 23:49:58 -0400

    Changed database to make default value for new game score 0,0 [#55951432]

---
+++ db/migrate/20130927034312_add_default_value_to_game_scores.rb
@@ -0,0 +1,7 @@
+class AddDefaultValueToGameScores < ActiveRecord::Migration
+  def change
+    change_column :games, :team_home_score, :integer, :default => 0
+    change_column :games, :team_away_score, :integer, :default => 0
+ 
+  end
+end
