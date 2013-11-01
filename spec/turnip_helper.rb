e748d4d4fbf981789ae0120894e96372826f58ff
Bradley <bradley.wittenbrook@gmail.com>
2013-11-01 10:11:45 -0400

    Attempt to fix travis build 2 [#59961116]

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -3,6 +3,16 @@ Dotenv.load
 require 'zonebie'
 Zonebie.set_random_timezone
 require 'turnip/capybara'
+Capybara.register_driver :selenium do |app|
+  profile = Selenium::WebDriver::Firefox::Profile.new
+
+  # Turn off the accessibility redirect popup
+  profile["network.http.prompt-temp-redirect"] = false
+
+  Capybara::Selenium::Driver.new(
+    app, :browser => :firefox, :profile => profile
+  )
+end
 Dir["spec/steps/**/*.rb"].each do |step_file|
     load step_file
 end
