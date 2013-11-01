48a367cfbdaada0749304d12ffb9910724c56b04
Bradley <bradley.wittenbrook@gmail.com>
2013-11-01 16:43:32 -0400

    It should not be this difficult. [#60003468]

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -3,7 +3,6 @@ Dotenv.load
 require 'zonebie'
 Zonebie.set_random_timezone
 require 'turnip/capybara'
-Capybara.javascript_driver = :webkit
 Dir["spec/steps/**/*.rb"].each do |step_file|
     load step_file
 end
