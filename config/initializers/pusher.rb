52c4cdb34d95fe5a423b15ca42a31a034007d3e3
Kurt Nelson <kurtisnelson@gmail.com>
2013-11-13 22:35:37 -0500

    Only do pusher in dev

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,6 @@
 require 'pusher'
 
-if ::Rails.env != "production"
+if ::Rails.env == "development"
   Pusher.app_id = '55244'
   Pusher.key    = 'e6f4f74ff01f5f5800af'
   Pusher.secret = ENV['PUSHER_SECRET']
